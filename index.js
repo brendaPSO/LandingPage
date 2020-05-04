51f4641ebfa0e29c946d45c87148872430983e4d
Brenda Orlandi <brenda_orlandi@hotmail.com>
2020-05-04 01:13:02 -0300

    Curso Full Stack

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
+
     $(".menu").click(function() {
         $("ul").toggleClass("active")
+    })
+
+});
+
+$(function($) {
+
+    $("form").submit(function(event) {
+
+        event.preventDefault();
+
+        $.ajax({
+            url: "https://formspree.io/mgeneagd",
+            method: "POST",
+            data: {
+                name: $("#nome").val(""),
+                email: $("#email").val(""),
+                message: $("#message").val("")
+            },
+            dataType: "json"
+        }).done(function() {
+            $("#nome").val(""),
+                $("#email").val(""),
+                $("#message").val(""),
+                alert("Email foi enviado com sucesso");
+        }).fail(function() {
+            alert("Erro ao enviar o email");
+        })
     });
 });
